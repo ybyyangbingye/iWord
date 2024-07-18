72a1fd5bd1295e3067166a6ceeb7870312a3bc8f
烨 <38682298+ybyyangbingye@users.noreply.github.com>
2024-07-19 04:51:03 +0800

    Add files via upload

---
+++ iWord/Extension.swift
@@ -0,0 +1,10 @@
+//
+//  Extension.swift
+//  iWord
+//
+//  Created by 何纪栋 on 2024/7/18.
+//
+
+import Foundation
+
+let kFruit: [String] = ["Apple","Orange","Banana","Banana","Watermelon","Pineapple","Peach","Peach"]
